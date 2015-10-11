ab27c15c914f06044839ff365109ab9240a536bd
yoursgayathri1 <santhanakrishnang@g.cofc.edu>
2015-10-11 18:07:15 +0000

    rails hw part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,11 @@
 class MoviesController < ApplicationController
-
+ @@count=0
   # See Section 4.5: Strong Parameters below for an explanation of this method:
   # http://guides.rubyonrails.org/action_controller_overview.html
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
+    
+     
   end
 
   def show
@@ -13,10 +15,77 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies=Movie.all()
-    @movies=@movies.order(params[:sort_by])
-    @sort_column = params[:sort_by]
+    
+   
   
+   @all_ratings = Movie.all_ratings    
+    redirect =false
+if params[:sort_by]
+@sort_by = params[:sort_by]
+session[:sort_by] = params[:sort_by]
+elsif session[:sort_by]
+@sort_by = session[:sort_by]
+redirect = true
+else
+@sort_by =nil
+end
+
+
+if params[:commit] =="Refresh" and params[:ratings].nil?
+@ratings =nil
+session[:ratings] =nil
+elsif params[:ratings]
+@ratings = params[:ratings]
+session[:ratings] =params[:ratings]
+elsif session[:ratings]
+@ratings =session[:ratings]
+redirect =true
+else
+@ratings =nil
+end
+
+if redirect
+flash.keep
+redirect_to movies_path :sort_by => @sort_by, :ratings => @ratings
+end
+
+   
+   
+   #########################################################################
+   
+    if @@count==0 
+       @@count=1
+    else @@count=2
+    end  
+     
+    @movies=Movie.all()
+    @movies=@movies.order(@sort_by)
+    @sort_column = @sort_by
+    
+#    @all_ratings= Movie.all_ratings
+    @temp=@ratings.to_a
+    @set_ratings=Hash.new()
+    @temp.each{ |x|
+    y=x[0]
+    @set_ratings[y] = 0}
+     
+  
+    if @set_ratings
+      @movies=Movie.where(rating: @set_ratings.keys).order(@sort_by)
+         @set_ratings.each { |x,y|
+         @set_ratings[x]=1 }
+    end
+   
+    if @@count==1
+       session.clear
+     @movies=Movie.all()
+     @all_ratings.each { |x|
+     @set_ratings[x] = 1
+     }
+    end
+    
+    
+    
     
   end
 
@@ -51,3 +120,5 @@ class MoviesController < ApplicationController
   private :movie_params
   
 end
+
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+ 
+
+    def self.all_ratings
+        @ratings=Array.new
+        self.select("rating").uniq.each {|x| @ratings.push(x.rating)}
+        puts @ratings
+        return  @ratings.sort.uniq
+    end
+    
 end
